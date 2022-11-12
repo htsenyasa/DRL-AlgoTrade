10631391bf1876fb90bc0e59bc5954c2c03a8826
htsenyasa <htsenyasa@gmail.com>
2022-11-12 12:13:50 +0300

    Update

--- src/tf_TradingOperations.py
+++ src/tf_TradingOperations.py
@@ -1,13 +1,35 @@
 import numpy as np
 
-class Position():
+signal = {"Buy": True, "Sell": False}
+
+class PositionHandler():
     """ Creates a position for a given stock """
-    def __init__(self, stockCode, initialLots = 0, initialCost = 0, currentPosition = None):
+    def __init__(self, stockCode, initialCash = 100_000, initialLots = 0, initialCost = 0, currentPosition = 0):
         self.stockCode = stockCode
-        self.lots = initialLots
-        self.initialCost = initialCost
-        self.currentPosition = currentPosition # Long or Short
-        self.history = []
+        self.cash = float(initialCash)
+        self.lots = float(initialLots)
+        self.cost = float(initialCost)
+        self.currentPosition = currentPosition # Long or Short or No Position
+
+        self.value = self.lots * self.cost
+        self.returns = 0
+        self.actionHistory = []
+
+        self.__long = 1
+        self.__noPosition = 0
+        self.__short = -1
+
+    def IsLong(self):
+        return self.currentPosition == self.__long
+    def IsShort(self):
+        return self.currentPosition == self.__short
+    def GetPosition(self):
+        return self.currentPosition 
+
+    def Buy(self, data, fee):
+        if self.IsLong():
+            ...
+
 
 
 class StockHandler():
@@ -20,7 +42,7 @@ class StockHandler():
         The main reason for these two functions to be different is that there may be a need to
         use different update function for simulation purposes.
     """
-    def __init__(self, stockCode, GetFunc, UpdateFunc, horizon):
+    def __init__(self, stockCode, GetFunc, UpdateFunc, horizon, interpolate=True):
         self.stockCode = stockCode
         self.GetFunc = GetFunc
         self.UpdateFunc = UpdateFunc
@@ -29,8 +51,10 @@ class StockHandler():
         self.interval = horizon.interval
 
         self.dataFrame = GetFunc(self.stockCode, start=self.startDate, end=self.endDate, interval=self.interval)
-        self.dataFrame.replace(0.0, np.nan, inplace=True)
-        self.dataFrame.interpolate(method='linear', limit=5, limit_area='inside', inplace=True)
-        self.dataFrame.fillna(method='ffill', inplace=True)
-        self.dataFrame.fillna(method='bfill', inplace=True)
-        self.dataFrame.fillna(0, inplace=True)
+        
+        if interpolate is True:
+            self.dataFrame.replace(0.0, np.nan, inplace=True)
+            self.dataFrame.interpolate(method='linear', limit=5, limit_area='inside', inplace=True)
+            self.dataFrame.fillna(method='ffill', inplace=True)
+            self.dataFrame.fillna(method='bfill', inplace=True)
+            self.dataFrame.fillna(0, inplace=True)
