638291306cd3f49e89b972644071b0ab6e35ff13
htsenyasa <htsenyasa@gmail.com>
2023-01-12 22:02:34 +0300

    main file change

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
+import multiprocessing as mp
 import yfinance as yf
 import tf_TradingOperations as to
 import ai_TradingEnvironment as te
 import ai_TDQN as tdqn
-import ai_Network as network
 import pandas as pd
 pd.options.mode.chained_assignment = None
 import torch
 import numpy as np
-import mplfinance as mpf
-import tf_DataAugmentation as da
-import matplotlib.pyplot as plt
 import random
-import time
-
+import os
 
 device = torch.device('cuda:'+str(0) if torch.cuda.is_available() else 'cpu')
 torch.manual_seed(0)
@@ -28,12 +24,12 @@ tdqnSettings = tdqn.tdqnSettings_(gamma=0.4,
                                   epsilonDecay=10000,
                                   capacity=100000, 
                                   learningRate=0.0001,
-                                  targetUpdateFrequency=1000, 
-                                  batchSize=32, 
+                                  targetUpdateFrequency=500, 
+                                  batchSize=128, 
                                   gradientClipping=1,
-                                  targetNetworkUpdate=1000, 
+                                  targetNetworkUpdate = 100, 
                                   alpha=0.1, 
-                                  numberOfEpisodes = 100, 
+                                  numberOfEpisodes = 50,
                                   rewardClipping = 1
                                   )
 
@@ -41,35 +37,82 @@ optimSettings = tdqn.optimSettings_(L2Factor=0.000001)
 
 
 startingDate = '2012-1-1'
-splittingDate = '2021-1-1'
-endingDate = '2023-1-1'
+splittingDate = '2018-1-1'
+endingDate = '2020-1-1'
 
 
 trainingHorizon = te.Horizon(startingDate, splittingDate, "1d")
 testingHorizon = te.Horizon(splittingDate, endingDate, "1d")
 
-aaplStockTraining = to.StockHandler("AAPL", yf.download, yf.download, trainingHorizon)
-aaplStockTesting = to.StockHandler("AAPL", yf.download, yf.download, testingHorizon)
 
-posTraining = to.DummyPosition(aaplStockTraining)
-posTesting = to.DummyPosition(aaplStockTesting)
+def ReadFromFile(stockName, start, end, interval, progress):
+    data = pd.read_csv("./Data/" + stockName, index_col=0)
+    data = data.loc[start:end]
+    data.index = pd.to_datetime(data.index)
+    return data
 
-trainingEnvironment = te.TradingEnvironment(posTraining)
-testingEnvironment = te.TradingEnvironment(posTesting)
 
-fileName = "APPL-2123-v1"
+def InitializeTrainingTesting(stockName, identifierString, verbose = False):
+    fileName = stockName + identifierString
+    modelFileName = "./Models/" + fileName + "/" + stockName
+    figureFileName = "./Figures/" + fileName + "/" + stockName
 
-start = time.time()
+    if verbose == True:
+        print("Stock Name: " + stockName + identifierString)
+    
+    StockTraining = to.StockHandler(stockName, yf.download, yf.download, trainingHorizon)
+    StockTesting = to.StockHandler(stockName, yf.download, yf.download, testingHorizon)
+    PositionTraining = to.DummyPosition(StockTraining)
+    PositionTesting = to.DummyPosition(StockTesting)
+    TrainingEnvironment = te.TradingEnvironment(PositionTraining)
+    TestingEnvironment = te.TradingEnvironment(PositionTesting)
+    Agent = tdqn.TDQNAgent(TrainingEnvironment, TestingEnvironment, tdqnSettings, networkSettings, optimSettings)
+    Agent.Training(verbose=True)
+    Agent.SaveModel(modelFileName)
+    Agent.Testing()
+    PositionTesting.PlotActionsCapital(figureFileName + "-Capital", showFlag=False)
+    PositionTesting.PlotActionsPrice(figureFileName + "-Price", showFlag=False)
+    Agent.PlotLoss(figureFileName + "-Loss", showFlag=False)
 
-agent = tdqn.TDQNAgent(trainingEnvironment, testingEnvironment, tdqnSettings, networkSettings, optimSettings)
-agent.Training(verbose=True)
-agent.SaveModel(fileName)
-# agent.LoadModel("MyModel3")
-agent.Testing()
 
-end = time.time()
-print("In Seconds: {}".format(end - start))
-print("In Minutes: {}".format((end - start)/60))
+def InitializeTesting(stockName, identifierString, verbose = False):
+    fileName = stockName + identifierString
+    modelFileName = "./Models/" + fileName + "/" + stockName
+    figureFileName = "./Figures/" + fileName + "/" + stockName
 
-posTesting.PlotActionsCapital(fileName + "-Capital", showFlag=True)
-agent.PlotLoss(fileName + "-Loss", showFlag=True)
+    if verbose == True:
+        print("Stock Name: " + stockName)
+    
+    StockTesting = to.StockHandler(stockName, yf.download, yf.download, testingHorizon)
+    PositionTesting = to.DummyPosition(StockTesting)
+    TestingEnvironment = te.TradingEnvironment(PositionTesting)
+    Agent = tdqn.TDQNAgent(TestingEnvironment, TestingEnvironment, tdqnSettings, networkSettings, optimSettings)
+    Agent.LoadModel(modelFileName)
+    Agent.Testing()
+    PositionTesting.PlotActionsCapital(figureFileName + "-Capital", showFlag=False)
+    PositionTesting.PlotActionsPrice(figureFileName + "-Price", showFlag=False)
+    Agent.PlotLoss(figureFileName + "-Loss", showFlag=False)
+
+
+if __name__ == "__main__":
+    mp.set_start_method('spawn')
+    listOfStocksNames = ["AAPL", "ISCTR.IS"]
+    identifier = "-1820-{}E-{}B-{}U".format(tdqnSettings.numberOfEpisodes, tdqnSettings.batchSize, tdqnSettings.targetNetworkUpdate)
+
+    paths = ["Models", "Figures"]
+
+    for path in paths:
+        for stockName in listOfStocksNames:
+            dirs = path + "/" + stockName + identifier
+            if not os.path.exists(dirs):
+                os.makedirs(dirs, exist_ok=True)
+
+    processList = [mp.Process(target=InitializeTrainingTesting, args = (listOfStocksNames[i], identifier, True)) for i in range(len(listOfStocksNames))]
+    
+    for process in processList:
+        process.start()
+
+    for process in processList:
+        process.join()
+
+
