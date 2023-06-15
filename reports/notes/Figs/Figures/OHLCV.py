926f4b2705acb1645dfa6d54c32a853c703576e9
htsenyasa <htsenyasa@gmail.com>
2023-06-15 11:51:42 +0300

    report update

--- reports/notes/Figs/Figures/OHLCV.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import mplfinance as mpf
-import matplotlib.pyplot as plt 
-
-data = yf.download("ISCTR.IS")[-5:]
-
-mpf.plot(data, type="candle", volume=True, savefig='OHLCV.png')
-
